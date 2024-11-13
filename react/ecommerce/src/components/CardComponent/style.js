6cf1e08788840b1434db685c0235861538b038bb
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-11-13 23:53:42 +0700

    Change-Study

---
+++ react/ecommerce/src/components/CardComponent/style.js
@@ -0,0 +1,47 @@
+import { Card } from "antd";
+import styled from "styled-components";
+export const WrapperCardStyle = styled(Card)`
+  width: 200px;
+  & img {
+    height: 200px;
+    width: 200px;
+  },
+  position: relative;
+`;
+
+export const WrapperImageStyle = styled.img`
+  top: -1px;
+  left: -1px;
+  border-top-left-radius: 3px;
+  position: absolute;
+  height: 14px;
+  width: 68px;
+`;
+
+export const StyleNameProduct = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: rgb(56, 56, 61);
+`;
+
+export const WrapperReportText = styled.div`
+  font-size: 10px;
+  color: rgb(128, 128, 137);
+  display: flex;
+  align-items: center;
+  margin: 6px 0;
+`;
+
+export const WrapperPriceText = styled.div`
+  color: rgb(255, 66, 78);
+  font-size: 16px;
+  font-weight: 500;
+  margin: 8px 0;
+`;
+
+export const WrapperDiscountText = styled.span`
+  color: rgb(255, 66, 78);
+  font-size: 12px;
+  font-weight: 500;
+`;
