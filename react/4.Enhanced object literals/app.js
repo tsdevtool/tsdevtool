575679edea7b213d44e0197e4e0b014d1f838722
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-11-15 00:22:52 +0700

    spread

--- react/4.Enhanced object literals/app.js
+++ react/4.Enhanced object literals/app.js
@@ -1,26 +1,50 @@
-// ENHANCED OBJECT LITERAL
-//1. Định nghĩa key value cho object
-//2. Định nghĩa method cho 1 object
-//3. Định nghĩa key cho object dưới dạng biến
+// SPREAD (...) : noi mang
+var array = ["JS", "Ruby", "PHP"];
+var array2 = ["ReactJS", "Dart"];
+var array3 = [...array2, ...array];
 
-var name = "JavaScript";
-var price = 2000;
+console.log(array3);
 
-var course = {
-  name,
-  price,
-  getName() {
-    return name;
-  },
+// Co the dung de thong nhat 2 obj
+
+var object1 = {
+  name: "JS",
 };
 
-console.log(course.getName());
-
-var filedName = "new-name";
-var filedPrice = "price";
-const course2 = {
-  [filedName]: "JavaScript",
-  [filedPrice]: 1000,
+var object2 = {
+  price: 1000,
 };
 
-console.log(course2);
+var object3 = { ...object1, ...object2 };
+
+console.log(object3);
+
+// Truyen tham so cho ham
+function logger(a, b, c) {
+  console.log(a, b, c);
+}
+
+logger(...array);
+
+//REST Parameters
+
+function logger(a, ...params) {
+  console.log(a, ...params);
+}
+function logger2({ name, ...rests }) {
+  console.log(name);
+  console.log(rests);
+}
+function logger3([a, b, ...rests]) {
+  console.log(a);
+  console.log(b);
+  console.log(rests);
+}
+
+logger(1, 2, 3, 4);
+logger2({
+  name: "JavaScript",
+  price: 1000,
+  description: "A description",
+});
+logger3([2, 6, 2, 3, 26, 1, 45, 56]);
