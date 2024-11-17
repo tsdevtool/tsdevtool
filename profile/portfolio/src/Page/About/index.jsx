107e3b1c2f647ac265721cc98182b076ae561f2b
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-11-18 02:28:30 +0700

    Done setup project react and tailwincss

---
+++ profile/portfolio/src/Page/About/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../..";
+const AboutPage = () => {
+  return (
+    <div className="mt-9 bg-red-400 w-full h-auto p-3 text-center text-white text-4xl hover:text-slate-500 font-bold">
+      AboutPage
+    </div>
+  );
+};
+
+export default AboutPage;
