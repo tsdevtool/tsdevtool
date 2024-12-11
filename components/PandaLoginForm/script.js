0d081976a99b7bb5e7d98588eeb22eeb99b9e641
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-11 13:15:29 +0700

    Done

---
+++ components/PandaLoginForm/script.js
@@ -0,0 +1,63 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const eyeL = document.querySelector(".eyeball-l");
+const eyeR = document.querySelector(".eyeball-r");
+const handL = document.querySelector(".hand-l");
+const handR = document.querySelector(".hand-r");
+const normalEyeStyle = () => {
+  eyeL.style.cssText = `
+    left:0.6em;
+    top: 0.6em;
+  `;
+  eyeR.style.cssText = `
+  right:0.6em;
+  top:0.6em;
+  `;
+};
+const normalHandStyle = () => {
+  handL.style.cssText = `
+        height: 2.81em;
+        top:8.4em;
+        left:7.5em;
+        transform: rotate(0deg);
+    `;
+  handR.style.cssText = `
+        height: 2.81em;
+        top: 8.4em;
+        right: 7.5em;
+        transform: rotate(0deg)
+    `;
+};
+username.addEventListener("focus", () => {
+  eyeL.style.cssText = `
+    left: 0.75em;
+    top: 1.12em;  
+  `;
+  eyeR.style.cssText = `
+    right: 0.75em;
+    top: 1.12em;
+  `;
+  normalHandStyle();
+});
+password.addEventListener("focus", () => {
+  handL.style.cssText = `
+        height: 6.56em;
+        top: 3.87em;
+        left: 11.75em;
+        transform: rotate(-155deg);    
+    `;
+  handR.style.cssText = `
+    height: 6.56em;
+    top: 3.87em;
+    right: 11.75em;
+    transform: rotate(155deg);
+  `;
+  normalEyeStyle();
+});
+document.addEventListener("click", e => {
+  let clickedElem = e.target;
+  if (clickedElem != username && clickedElem != password) {
+    normalEyeStyle();
+    normalHandStyle();
+  }
+});
