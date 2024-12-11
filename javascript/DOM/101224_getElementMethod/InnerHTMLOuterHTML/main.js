23cda3e3329b6e86ab9937a99cc8105fb3b2c3d9
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-11 07:41:12 +0700

    attribute, innerText, textContent

---
+++ javascript/DOM/101224_getElementMethod/InnerHTMLOuterHTML/main.js
@@ -0,0 +1,12 @@
+
+//innnerText, textContent
+
+//Attributes Node
+var h1Element = document.querySelector('h1');
+
+h1Element.setAttribute('title' , 'F8 - Học lập trình để đi làm');
+h1Element.setAttribute("data-title","F8 - Học lập trình để đi làm");
+
+var aElement = document.querySelector('a');
+aElement.href = 'https://fullstack.edu.vn/';
+aElement.target = '_blank';
