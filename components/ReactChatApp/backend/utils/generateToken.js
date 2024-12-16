c6a0a28b22df25b21dc5e30f55d40cf34c8c0ce0
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-16 19:32:42 +0700

    Login End Point

--- components/ReactChatApp/backend/utils/generateToken.js
+++ components/ReactChatApp/backend/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateTokenAndSetCookie =(userId, res)=>{
     res.cookie("jwt",token,{
         maxAge: 15*24*60*60*1000,
         httpOnly: true, //prevet XSS acttacjs cross-site scripting attacks
-        sameSite:"strict" //CSRF attacks cross-site request forgery attacks
+        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !== "development"
     })
 }
