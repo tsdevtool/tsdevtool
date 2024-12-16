6f370cb0ac9fe970e8f8bd78eeab01b75f062caa
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-16 16:40:45 +0700

    SignupEndpoint

--- components/ReactChatApp/backend/routes/auth.routes.js
+++ components/ReactChatApp/backend/routes/auth.routes.js
@@ -3,10 +3,10 @@ import {login, logout, signup} from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
-router.get("/login", login);
+// router.get("/login", login);
 
-router.get("/logout", logout); 
+// router.get("/logout", logout); 
 
-router.get("/signup", signup);
+router.post("/signup", signup);
 
 export default router;
--- components/ReactChatApp/backend/server.js
+++ components/ReactChatApp/backend/server.js
@@ -4,17 +4,22 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+dotenv.config();
 
-    //root route http://localhost:5000
-    res.send('Hello, World!')
-})
+app.use(express.json());  // for parsing json requests
 
 app.use("/api/auth", authRoutes);
+
+
+// app.get('/', (req, res) => {
+
+//     //root route http://localhost:5000
+//     res.send('Hello, World!')
+// })
+
+
 
 app.listen(PORT, () =>{ 
     connectToMongoDB();
