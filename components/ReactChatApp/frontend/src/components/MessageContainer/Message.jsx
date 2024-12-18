fbe3a114091a2ad4df0c76f712bb9c552c69295d
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-18 23:18:12 +0700

    GetMessages

--- components/ReactChatApp/frontend/src/components/MessageContainer/Message.jsx
+++ components/ReactChatApp/frontend/src/components/MessageContainer/Message.jsx
@@ -1,16 +1,31 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext.jsx";
+import useConversation from "../../zustand/useConvesation.js";
+import { extractTime } from "../../utils/extractTime.js";
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const nameAvatar = fromMe ? "You" : selectedConversation.fullName;
+  const fommattedTime = extractTime(message.createdAt);
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src="" alt="Tailwin CSS chat bubble component" />
+          <img src={profilePic} alt={`Avatar of ${nameAvatar}`} />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Hi! What is upp?
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
       </div>
-      <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>
-        12:42
+      <div
+        className={`chat-footer opacity-50 text-xs flex gap-1 items-center text-white`}
+      >
+        {fommattedTime}
       </div>
     </div>
   );
