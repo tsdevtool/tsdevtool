b31501d95ef2bccfd655088b1a0d152ad87e0fff
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-18 14:21:47 +0700

    Get Conversation

--- components/ReactChatApp/frontend/src/components/Sibar/Conversation.jsx
+++ components/ReactChatApp/frontend/src/components/Sibar/Conversation.jsx
@@ -1,20 +1,30 @@
-const Conversation = () => {
+import useConversation from "../../zustand/useConvesation.js";
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-2 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-2 cursor-pointer ${
+          isSelected ? "bg-sky-500" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src="#" alt="User avatar" />
+            <img src={conversation.profilePic} alt="User avatar" />
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">Sieu Nguyen Thanh</p>
-            <span className="text-xl">🥲</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
-      <div className="divider my-0 py-0 h-1"></div>
+      {!lastIdx && <div className="divider my-0 py-0 h-1"></div>}
     </>
   );
 };
