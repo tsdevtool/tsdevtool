0e5e446c8d9f5b3d138f53e26d104b9ceb5c7ce9
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2024-12-19 01:36:06 +0700

    Deployment

--- components/ReactChatApp/backend/server.js
+++ components/ReactChatApp/backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -8,6 +9,7 @@ import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,6 +20,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // app.get('/', (req, res) => {
 
 //     //root route http://localhost:5000
